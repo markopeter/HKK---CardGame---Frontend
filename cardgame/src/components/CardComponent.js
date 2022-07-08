11a84aae5b6c461b8f6416f45f4bda57dc126c46
Péter Markó <peter.marko1089@gmail.com>
2022-07-08 18:51:18 +0200

    Add: Add card details with the proper placement.

--- cardgame/src/components/CardComponent.js
+++ cardgame/src/components/CardComponent.js
@@ -3,10 +3,16 @@ import React from 'react';
 const CardComponent = (props) => {
     return (
         <div>
+            <div className="cardContainer">
+                <div className="cardUpperRight">{props.name}</div>
+                <div className="cardUpperLeft">Manacost :  {props.manaCost}</div>
+                <div className="cardBottomLeft">Attack : {props.baseAttack}</div>
+                <div className="cardBottomRight">Health : {props.baseHealth}</div>
             <div className="imageContainer">
                 < img src={props.imageUrl} alt="new"  />
-                <div className="nameContainer"><p>{props.name}</p></div>
+                <div className="nameContainer"><p>{props.description}</p></div>
             </div>
+                </div>
         </div>
     );
 };
