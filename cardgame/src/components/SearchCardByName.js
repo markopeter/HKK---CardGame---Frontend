61621415ed8246c18274bd271d854cb730c92818
Péter Markó <peter.marko1089@gmail.com>
2022-07-08 09:21:54 +0200

    Add: Single card search happening by axios get request.

--- cardgame/src/components/SearchCardByName.js
+++ cardgame/src/components/SearchCardByName.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+
 
 const SearchCardByName = () => {
+
+    const [inputName, setInputName] = useState("");
+    const [cardData, setCardData] = useState({});
+    const url = `https://hkk-petproject.herokuapp.com/creature/${inputName}`;
+    const searchByName = () => {
+
+        axios.get(url, {params:
+                {
+                    name : inputName}})
+            .then(r => setCardData(r.data));
+
+    }
+    useEffect(() => { searchByName()},[]);
     return (
         <div>
-            
+            <input  type="text" id="message" name="name"
+                   value={inputName}
+                   onChange={(event) => setInputName(event.target.value)}
+                   autoComplete="off"
+            />
+            <button onClick={searchByName}>Search ! </button>
+            <div>
+                <h1>{cardData.name}</h1>
+            </div>
         </div>
     );
 };
