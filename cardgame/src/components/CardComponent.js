eacd5e2647f31d661835e52ba8139a6ac9cbfd33
Péter Markó <peter.marko1089@gmail.com>
2022-07-08 12:46:33 +0200

    Add: h1 and img tag with the correct properties

--- cardgame/src/components/CardComponent.js
+++ cardgame/src/components/CardComponent.js
@@ -3,7 +3,8 @@ import React from 'react';
 const CardComponent = (props) => {
     return (
         <div>
-
+            <h1>{props.name}</h1>
+            < img src={props.imageUrl} alt="new"  />
         </div>
     );
 };
