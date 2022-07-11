9408bef6dbc257cb666262aa418245a40ddcf77f
Péter Markó <peter.marko1089@gmail.com>
2022-07-11 23:31:34 +0200

    Add: Button and TextField component.'

--- cardgame/src/components/SearchCardByName.js
+++ cardgame/src/components/SearchCardByName.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import CardComponent from "./CardComponent";
-
+import Button from '@mui/material/Button';
+import SearchResult from "./SearchResult";
+import {TextField} from "@mui/material";
 
 const SearchCardByName = () => {
 
@@ -14,19 +16,21 @@ const SearchCardByName = () => {
                 {
                     name : inputName}})
             .then(r => setCardData(r.data));
-
     }
     useEffect(() => { searchByName()},[]);
     return (
         <div>
-            <input  type="text" id="message" name="name"
+            <br/>
+            <TextField label="Filled" variant="filled"  type="text" id="message" name="name"
                    value={inputName}
                    onChange={(event) => setInputName(event.target.value)}
                    autoComplete="off"
             />
-            <button onClick={searchByName}>Search ! </button>
+            <Button variant="contained" onClick={searchByName}>Search ! </Button>
             <div>
-                {/*<CardComponent data={cardData} />*/}
+                <p>Base attack: {cardData.baseAttack}</p>
+                <p>Base health: {cardData.baseHealth}</p>
+                <p>Manacost: {cardData.manaCost}</p>
             </div>
         </div>
     );
