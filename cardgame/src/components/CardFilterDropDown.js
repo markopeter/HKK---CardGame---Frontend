fd2e1b250aeb010345c36466f1da1d622f564b63
Péter Markó <peter.marko1089@gmail.com>
2022-07-11 20:27:12 +0200

    Add: Options constant, SearchResult component,  Filter card button and input field for the 2. filtering value.'

--- cardgame/src/components/CardFilterDropDown.js
+++ cardgame/src/components/CardFilterDropDown.js
@@ -1,20 +1,46 @@
 import React, {useState} from 'react';
+import axios from "axios";
+import SearchResult from "./SearchResult";
 
 const CardFilterDropDown = () => {
-
-    const [detail, setDetail] = useState('');
+    const options = [
+        { value: 'name', label: 'Name' },
+        { value: 'manaCost', label: 'Mana Cost' },
+        { value: 'baseAttack', label: 'Base Attack' },
+        { value: 'baseHealth', label: 'Base Health' }
+    ]
+    const [cardData, setCardData] = useState([]);
+    const [detail, setDetail] = useState(options[0].value);
+    const [inputValue, setInputValue] = useState('');
+    const url = `https://hkk-petproject.herokuapp.com/creature/detail/${detail}/value/${inputValue}`;
+    const fetchCards = () => {
+        axios.get(url, {params: {detail: detail, value: inputValue}})
+            .then(r => setCardData(r.data));
+    }
     const changeDetail = (event) =>{
-        event.target(setDetail);
+        console.log(event.target.value);
+        setDetail(event.target.value);
     }
     return (
         <div >
             <select value={detail} onChange={changeDetail}>
-                <option value="name">Name</option>
-                <option value="manaCost">ManaCost</option>
-                <option value="baseAttack">ManaCost</option>
-                <option value="baseHealth">ManaCost</option>
+                {options.map(option => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
             </select>
-
+            <div>
+                <button onClick = {fetchCards} > Filter cards ! </button>
+                <input  type="text" id="inputValue" name="name"
+                        value={inputValue}
+                        onChange={(event) => setInputValue(event.target.value)}
+                        autoComplete="off"
+                />
+            </div>
+            <div>
+                <SearchResult data={cardData}/>
+            </div>
         </div>
     );
 };
