15a6f76726b071c113746ff378a4b4af0f0e6756
Péter Markó <peter.marko1089@gmail.com>
2022-07-12 17:20:26 +0200

    Add: DeckCards component responsible to display all the card which belongs to the choosen deck.'

---
+++ cardgame/src/components/DeckCards.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import Button from "@mui/material/Button";
+import axios from "axios";
+import {useEffect} from "react";
+
+const DeckCards = (props) => {
+    const url = `https://hkk-petproject.herokuapp.com/deck/${props.data.deckName}`;
+    const [deckCards, setDeckCards] = useState([]);
+    const [isShown, setIsShown] = useState(false);
+    const fetchDeckCard = () => {
+        console.log(props);
+        axios.get(url, {params:
+                {
+                    name : props.data.deckName}})
+            .then(r => setDeckCards(r.data));
+        setIsShown(current => !current);
+    }
+    useEffect(() => { fetchDeckCard()},[]);
+    return (
+        <div>
+
+            <Button variant="contained" onClick={fetchDeckCard} >Show this deck cards ! </Button>
+            {isShown &&
+            <div>
+                {deckCards && deckCards.map(element => {
+                    return (
+                        <div> {element.name}</div>
+                    )
+                })}
+            </div>}
+        </div>
+    );
+};
+
+export default DeckCards;
