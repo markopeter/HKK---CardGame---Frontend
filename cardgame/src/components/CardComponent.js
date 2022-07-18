b7e4f47d1f5af36e16de6940393cf2dd8680f36e
Péter Markó <peter.marko1089@gmail.com>
2022-07-18 09:56:49 +0200

    Add: Necessary properties and css classes to the CardComponent.'

--- cardgame/src/components/CardComponent.js
+++ cardgame/src/components/CardComponent.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const CardComponent = (props) => {
-    console.log(props);
     return (
         <div>
-            <div key={props.id} className="imageContainer">
-                < img src={props.data.imageUrl} alt="new"  />
-                <div className="nameContainer"><p>{props.data.name}</p></div>
+            <div className="cardContainer">
+                <div className="cardUpperRight">{props.data.name}</div>
+                <div className="cardUpperLeft">Manacost :  {props.data.manaCost}</div>
+                <div className="cardBottomLeft">Attack : {props.data.baseAttack}</div>
+                <div className="cardBottomRight">Health : {props.data.baseHealth}</div>
+                <div className="imageContainer">
+                    < img src={props.data.imageUrl} alt="new"  />
+                    <div><p>{props.data.description}</p></div>
+                </div>
             </div>
         </div>
     );
 };
 
 export default CardComponent;
-
