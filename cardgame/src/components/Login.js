3bfd1a3caebb5b3ac9f1cdaa8d4928fe1268c750
Péter Markó <peter.marko1089@gmail.com>
2022-08-19 15:24:59 +0200

    Add: basic login with jwt token.'

---
+++ cardgame/src/components/Login.js
@@ -0,0 +1,67 @@
+
+import React, { useState } from "react";
+import { useLocalState } from "../util/useLocalStorage";
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [jwt, setJwt] = useLocalState("", "jwt");
+
+    function sendLoginRequest() {
+        const reqBody = {
+            username: username,
+            password: password,
+        };
+
+        fetch("/api/auth/login", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "post",
+            body: JSON.stringify(reqBody),
+        })
+            .then((response) => {
+                if (response.status === 200)
+                    return Promise.all([response.json(), response.headers]);
+                else return Promise.reject("Invalid login attempt");
+            })
+            .then(([body, headers]) => {
+                setJwt(headers.get("Authorization"));
+            })
+            .catch((message) => {
+                alert(message);
+                console.log(username);
+                console.log(password);
+            });
+    }
+    return (
+        <>
+            <div>
+                <label htmlFor="username">Username</label>
+                <input
+                    type="email"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div>
+                <button id="submit" type="button" onClick={() => sendLoginRequest()}>
+                    Login
+                </button>
+            </div>
+        </>
+    );
+
+};
+export default Login;
