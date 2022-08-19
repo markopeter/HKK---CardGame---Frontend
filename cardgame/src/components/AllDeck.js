427273682d1c13fc1e005288d9e9df21867ee4c9
Péter Markó <peter.marko1089@gmail.com>
2022-08-19 15:22:51 +0200

    Modify: change axios post request to fetch post request with proper jwt flow.'

--- cardgame/src/components/AllDeck.js
+++ cardgame/src/components/AllDeck.js
@@ -3,15 +3,18 @@ import Button from "@mui/material/Button";
 import axios from "axios";
 import {List, ListItem, ListItemButton, ListItemText, TextField} from "@mui/material";
 import DeckCards from "./DeckCards";
+import {useLocalState} from "../util/useLocalStorage";
 
 const url = `https://hkk-petproject.herokuapp.com/deck`;
+const localUrl = `/api/deck`
 const fetchDecks = () => {
-    return axios.get(url);
+    return axios.get(localUrl);
 }
 const AllDeck = () => {
     const urlPost = `https://hkk-petproject.herokuapp.com/deck/create`;
     const [inputValue, setInputValue] = useState('');
     const [decks, setDecks] = useState([]);
+    const [jwt, setJwt] =  useLocalState("", "jwt");
     const showCards = () => {
         fetchDecks()
             .then((response) => {
@@ -19,21 +22,31 @@ const AllDeck = () => {
                 setDecks(data);
             });
     }
-    const postData = (e) => {
-        e.preventDefault();
-        axios.post(urlPost, {
-            name: inputValue
-        }).then(() => {
-            showCards();
+
+    function createDeck( ) {
+        fetch("/api/deck/create",
+            {
+                headers: {
+                    "content-type": "application/json",
+                    Authorization: `Bearer ${jwt}`
+                },
+                method: "POST",
+                body: JSON.stringify({ name: inputValue })
+
+            }).then(response => {
+                if(response.status === 200) return response.json();
+        }).then(data => {
+            console.log("Git gud");
         })
-    }
+     }
+
     useEffect(() => {
         showCards();
     }, []);
 
     const deleteDeck =  id => {
         try {
-             axios.delete(`${url}/${id}`).then(() => showCards());
+             axios.delete(`${localUrl}/${id}`).then(() => showCards());
              showCards();
         } catch (error) {
             alert(error)
@@ -67,7 +80,7 @@ const AllDeck = () => {
             </div>
             <div>
                 <div>
-                    <Button variant="contained" onClick={postData}>Add new deck ! </Button>
+                    <Button variant="contained" onClick={createDeck}>Add new deck ! </Button>
                     <TextField  id="inputValue" label="Filled" variant="filled" type="text" name="name"
                                 value={inputValue}
                                 onChange={(event) => setInputValue(event.target.value)}
